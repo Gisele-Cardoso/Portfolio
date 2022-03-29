89c2db834b2b8dea6c3aae5225ca2dccc5cef4b5
Gisele Cardoso <93274363+Gisele-Cardoso@users.noreply.github.com>
2022-03-29 20:44:47 -0300

    atual

--- script.js
+++ script.js
@@ -64,17 +64,4 @@ form.addEventListener("submit", function (e) {
       }, 5000);
     });
 });
-
-//scroll
-const sr = ScrollReveal({
-  origin: 'top',
-  distance: '60px',
-  duration: 2000,
-  delay: 200,
-//     reset: true
-});
-
-sr.reveal('.home-img, .home-texto, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+ 
